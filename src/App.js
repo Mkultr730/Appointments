27c04ee3de41e1881c42c13c88d28364ad0b8778
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-07-14 16:23:24 -0500

    feat: Form validations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Form from './components/Form'
 
 function App() {
+
+  // appointments
+  const [appointments, setAppointments] = useState([]);
+
+  const createAppointment = (appointment) => {
+    setAppointments([
+      ...appointments,
+      appointment
+    ]);
+  }
+
   return (
     <Fragment>
     <h1>Patients Managements</h1>
 
     <div className="container">
-      <div class="row">
+      <div className="row">
         <div className="one-half column">
-          <Form />
+          <Form 
+            createAppointment={createAppointment}
+          />
         </div>
         <div className="one-half column">
           2
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,71 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-const Form = () => {
+const Form = ({createAppointment}) => {
+
+    const [appointment, setAppointment] = useState({
+        pet: '',
+        owner: '',
+        date: '',
+        time: '',
+        symptoms: ''
+    });
+
+    const [error, setError] = useState(false);
+
+    const updateState = (e) => {
+        setAppointment({
+            ...appointment,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const { pet, owner, date, time, symptoms } = appointment;
+
+    const submitAppointmet = (e) => {
+        e.preventDefault();
+
+        // Validations
+        if (pet.trim() === '' || owner.trim() === '' || date.trim() === '' || time.trim() === '' || symptoms.trim() === '') {
+            setError(true);
+            return;
+        }
+        setError(false);
+
+        // ID
+        appointment.id = uuidv4();
+
+        // Create Appointment
+        createAppointment(appointment);
+
+        //Refresh Form
+        setAppointment({
+            pet: '',
+            owner: '',
+            date: '',
+            time: '',
+            symptoms: ''
+        });
+    }
+
+
     return (
         <Fragment>
             <h2>Book appointment</h2>
 
-            <form>
+            { error ? <p className="alerta-error">All fields are requiered</p> : null }
+
+            <form
+                onSubmit={submitAppointmet}
+            >
                 <label>Pet's Name</label>
                 <input
                     type="text"
                     name="pet"
                     className="u-full-width"
                     placeholder="Pet's name"
+                    onChange={updateState}
+                    value={ pet }
                 />
 
                 <label>Owner's Name</label>
@@ -20,28 +74,35 @@ const Form = () => {
                     name="owner"
                     className="u-full-width"
                     placeholder="Owner's name"
+                    onChange={updateState}
+                    value={ owner }
                 />
                 <label>Date</label>
                 <input
                     type="date"
                     name="date"
                     className="u-full-width"
+                    onChange={updateState}
+                    value={ date }
                 />
                 <label>Time</label>
                 <input
                     type="time"
                     name="time"
                     className="u-full-width"
+                    onChange={updateState}
+                    value={ time }
                 />
                 <label>Symptoms</label>
                 <textarea
                     className="u-full-width"
                     name="symptoms"
+                    onChange={updateState}
+                    value={ symptoms }
                 ></textarea>
                 
                 <button 
-                    type="submit" 
-                    onClick="" 
+                    type="submit"
                     className="u-full-width button-primary"
                 >Make appointment</button>
 
